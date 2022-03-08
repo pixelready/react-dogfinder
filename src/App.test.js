f170aa672ebaa07ef2369a4b71c913b9f5e2601d
Ryan Nissen <ryankn852@gmail.com>
2022-03-07 18:17:14 -0600

    Working on test. Trouble with await during testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,25 @@
 import Router from "react-router";
 Router.useParams = jest.fn();
 import DogDetails from './DogDetails';
+import { MemoryRouter } from 'react-router-dom';
+import App from './App';
+
 
 import { render, screen, waitFor } from '@testing-library/react';
 
-test('renders learn react link', () => {
-  Router.useParams.mockReturnValue({a: "b"});	
-  render(<DogDetail />)
-});
+// test('renders learn react link', () => {
+//   Router.useParams.mockReturnValue({a: "b"});	
+//   render(<DogDetails />)
+// });
+
+it('renders the homepage/doglist page', async function () {
+  const { getByText } = await Promise.resolve(render(
+    <MemoryRouter initialEntries={['/']}>
+      <App />  
+    </MemoryRouter>
+  ));
+  
+  const pText = getByText('Loading...');
+  expect(pText).toBeInTheDocument();
+})
+
